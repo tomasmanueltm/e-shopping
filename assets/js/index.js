e2270ab36dd8a5dfb663e0536008acaccec05e46
TomasManueltm <antoniomanuelbaptistabaptista@gmail.com>
2022-07-25 04:07:44 +0100

    objetivo scroll alcançado

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,42 @@
-let $ = (e)=> document.querySelector(e),
-    left = $('.left'),
-    carousel = $('.items'),
-    right = $('.right');
-    
-left.addEventListener('click', function(e){
-    e.preventDefault();
-    carousel.scrollLeft -= carousel.getBoundingClientRect().width;
-   
+let JS = (e)=> document.querySelector(e),
+    JSall = (c)=> document.querySelectorAll(c), 
+    btnNext = JSall('.indicador-next'),
+    carousel = JS('.catalog ul'),
+    carouselLi = JSall('.catalog ul li');
+    // next = $('.indicador-next');
+  
+function removeTag()
+{
+    carouselLi.forEach(element => {
+        element.classList.remove("a");
+    });
+}
+btnNext.forEach( (btn)=> {
+    btn.addEventListener('click', function(e){
+        e.preventDefault();
+        removeTag();
+        
+        if(carousel.scrollLeft == 0 || carousel.scrollLeft != 0)
+        {
+            carousel.scrollLeft += 870;
+            btn.parentElement.classList.add("a");
+        }
+        
+        if(carousel.scrollLeft == 1587)
+        {
+            carousel.scrollLeft = 0;
+            carouselLi[0].classList.add("a");
+        }
+    })
 })
 
-right.addEventListener('click', function(e){
-    e.preventDefault();
-    carousel.scrollLeft += carousel.getBoundingClientRect().width;
-})
+// function reverter()
+// {
+//     [...carouselLi].shift();
+// }
+
+
+// next.addEventListener('click', function(e){
+//     e.preventDefault();
+//     carousel.scrollLeft -= carousel.getBoundingClientRect().width;
+// })
